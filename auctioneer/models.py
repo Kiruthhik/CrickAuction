a7732ba6bfd3a9c57baa9017f4afaf3eec1b43a1
Kiruthhik-A-S <kiruthhik@gmail.com>
2024-11-01 19:24:05 +0530

    auction participants inclusion

--- auctioneer/models.py
+++ auctioneer/models.py
@@ -19,14 +19,14 @@ class AuctionFranchise(models.Model):
     purse = models.DecimalField(max_digits=30, decimal_places=0, default=None, null=True)
 
     def __str__(self):
-        return f'{self.auction.name} in {self.original_franchise.user.username}'
+        return f'{self.original_franchise.user.username} in {self.auction.name}'
 
 class AuctionPlayer(models.Model):
     auction = models.ForeignKey(LiveAuction, on_delete=models.CASCADE , related_name='auction_players')
     original_profile = models.ForeignKey(PlayerProfile, on_delete=models.CASCADE, related_name='auction_entries')
     bid = models.DecimalField(max_digits=15 ,decimal_places=0, default=None , null=True)
     team = models.CharField(max_length=20, default=None, null=True)
-    interested_team = models.ForeignKey(AuctionFranchise, on_delete=models.SET_NULL ,null=True,blank=True, related_name="interested_in")
+    interested_team = models.ManyToManyField(AuctionFranchise,null=True,blank=True, related_name="interested_in")
 
     def __str__(self):
-        return f'{self.original_profile.user.username in self.auction.name}'
+        return f'{self.original_profile.user.username} in {self.auction.name}'
--- auctioneer/views.py
+++ auctioneer/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from .models import *
 from players.models import PlayerProfile
 from franchise.models import FranchiseProfile
+from django.http import JsonResponse
 # Create your views here.
 
 def login(request):
@@ -32,8 +33,30 @@ def index(request):
     return render(request,'auctioneer_index.html')
 
 def add(request,auc_name):
+    auction = LiveAuction.objects.get(name=auc_name)
     if request.method == 'POST':
-        pass
+        import json
+        data = json.loads(request.body.decode('utf-8'))
+        name = data.get('name')
+        type = data.get('type')
+        try:
+            if type == 'franchise':
+                franchise = FranchiseProfile.objects.get(user__username=name)
+                purse = data.get('purse')
+                AuctionFranchise.objects.create(auction=auction,original_franchise=franchise,purse=purse).save
+            elif type == 'player':
+                player = PlayerProfile.objects.get(user__first_name=name)
+                auc_player = AuctionPlayer.objects.create(auction=auction,original_profile=player)
+                teams = list(AuctionFranchise.objects.filter(auction=auction))
+                for team in teams:
+                    auc_player.interested_team.add(team)
+            return JsonResponse({'success': True, 'message': f'{type} {name} added successfully.'})
+        
+        except FranchiseProfile.DoesNotExist:
+            return JsonResponse({'success': False, 'message': 'Franchise not found.'})
+        
+        except PlayerProfile.DoesNotExist:
+            return JsonResponse({'success': False, 'message': 'Player not found.'})
     franchises = list(FranchiseProfile.objects.all())
     players = list(PlayerProfile.objects.all())
     content = {
